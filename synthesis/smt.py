31cb6b3eae861f81a7cb432cff1152364f26e12c
Divyanshu Saxena <dssaxena2011@gmail.com>
2023-04-30 18:50:14 -0500

    Add Breadth-first traversal based policy context expansion for * operator

--- synthesis/smt.py
+++ synthesis/smt.py
@@ -18,45 +18,123 @@ policy_constraints = {
 
 # Define the application graph
 def define_appl_graph():
-    nodes = ["A", "B", "C", "D", "E"]
-    edges = {"A": ["B", "C"], "B": ["D"], "C": ["D"], "D": ["E"]}
+    nodes = ["A", "B", "C", "D", "E", "F", "G"]
+    edges = {
+        "A": ["B", "C"],
+        "B": ["E"],
+        "C": ["D"],
+        "D": ["E"],
+        "E": ["F", "G"]
+    }
     return nodes, edges
 
 
 # TODO: Handle multiple functions in a single policy.
 def define_user_policies():
     policies = [{
-        "context": ["A", "B"],
-        "function": Policies.SetDeadline
+        "context": ["A", "*"],
+        "function": Policies.SetHeader
     }, {
-        "context": ["A", "C"],
+        "context": ["*", "F"],
         "function": Policies.Count
     }, {
-        "context": ["A", "*", "D"],
+        "context": ["A", "*", "E", "*"],
         "function": Policies.SetDeadline
     }]
     return policies
 
 
-# TODO: Only considers a single variable for * operator.
-def expand_policy_context(policy_context, appl_edges):
-    # Initially request_contexts would just be the first node.
-    print("Expanding policy context ", policy_context)
-    curr_context_queue = [[policy_context[0]]]
-    next_context_queue = []
-    for n in policy_context[1:]:
-        if n == "*":
-            while curr_context_queue:
-                curr_context = curr_context_queue.pop(0)
-                for e in appl_edges[curr_context[-1]]:
-                    next_context_queue.append(curr_context + [e])
+# Start from a node and get all paths that satisfy the policy context.
+def forward_policy_context(policy_context, appl_edges):
+    curr_context_list = [[policy_context[0]]]
+    prev_node = policy_context[0]
+    for i in range(1, len(policy_context)):
+        if policy_context[i] != "*":
+            prev_node = policy_context[i]
+            for context in curr_context_list:
+                context.append(policy_context[i])
         else:
-            while curr_context_queue:
-                curr_context = curr_context_queue.pop(0)
-                next_context_queue.append(curr_context + [n])
-        curr_context_queue = next_context_queue
-        next_context_queue = []
-    return curr_context_queue
+            target_node = policy_context[
+                i + 1] if i < len(policy_context) - 1 else None
+            if target_node:
+                bft_queue = [[n] for n in appl_edges[prev_node]]
+
+                # Keep track of paths from previous node to target node.
+                new_context_list = []
+                while bft_queue:
+                    # print("BFT Queue: ", bft_queue)
+                    # print("Current Context List: ", curr_context_list)
+                    curr_path = bft_queue.pop(0)
+                    curr_node = curr_path[-1]
+
+                    # Unroll BFS until target node is found or a leaf is met.
+                    if curr_node == target_node:
+                        for context in curr_context_list:
+                            new_context_list.append(context + curr_path[:-1])
+                    elif curr_node in appl_edges:
+                        # Add the children to bft queue.
+                        for n in appl_edges[curr_node]:
+                            bft_queue.append(curr_path + [n])
+                curr_context_list = new_context_list
+            else:
+                # If target node is None, then just add all the children of the previous node.
+                new_context_list = []
+                for context in curr_context_list:
+                    for n in appl_edges[prev_node]:
+                        new_context_list.append(context + [n])
+                curr_context_list = new_context_list
+
+    return curr_context_list
+
+
+# Get all paths that satisfy the policy context and end on the given policy_context.
+def backward_policy_context(target_node, appl_edges):
+    # Construct the parent-child edges from appl_edges.
+    parents = {}
+    for n, e in appl_edges.items():
+        for c in e:
+            if c in parents:
+                parents[c].append(n)
+            else:
+                parents[c] = [n]
+    print(parents)
+
+    backward_bft_queue = [[target_node]]
+    context_list = []
+    while backward_bft_queue:
+        # print("BFT Queue: ", bft_queue)
+        # print("Current Context List: ", curr_context_list)
+        curr_path = backward_bft_queue.pop(0)
+        curr_node = curr_path[0]
+        if len(curr_path) > 1:
+            context_list.append(curr_path)
+
+        # Unroll BFS until a root node is met.
+        if curr_node in parents:
+            # Add the children to bft queue.
+            for n in parents[curr_node]:
+                backward_bft_queue.append([n] + curr_path)
+
+    return context_list
+
+
+# Expand the policy context to get all possible request contexts.
+def expand_policy_context(policy_context, appl_edges):
+    if policy_context[0] != "*":
+        return forward_policy_context(policy_context, appl_edges)
+    else:
+        pre_context_list = backward_policy_context(policy_context[1],
+                                                   appl_edges)
+        post_context_list = forward_policy_context(policy_context[1:],
+                                                   appl_edges)
+        context_list = []
+        for pre_context in pre_context_list:
+            if post_context_list:
+                for post_context in post_context_list:
+                    context_list.append(pre_context + post_context[1:])
+            else:
+                context_list.append(pre_context)
+        return context_list
 
 
 # TODO: Only considers a single variable for * operator.
@@ -86,17 +164,18 @@ def main():
     user_policies = define_user_policies()
 
     # Define the objective
-    objective = 3
+    objective = 2
 
     # Expand the policy contexts
     all_req_contexts = {}
     for i in range(len(user_policies)):
         policy = user_policies[i]
         req_contexts = expand_policy_context(policy["context"], appl_edges)
+        print("Expanding policy context ", policy["context"])
+        print("Expanded contexts: ", req_contexts)
+
         for req_context in req_contexts:
             if tuple(req_context) in all_req_contexts:
-                # TODO: Handle duplicate contexts.
-                print("WARN: Duplicate contexts")
                 all_req_contexts[tuple(req_context)].append(i)
             else:
                 all_req_contexts[tuple(req_context)] = [i]
@@ -111,21 +190,21 @@ def main():
     # Define the "Belong to the policy context" variables
     B = [[z3.Bool("b_{}_{}".format(i, j)) for j in range(num_policies)]
          for i in range(num_req_contexts)]
-    print(B)
+    # print(B)
 
     # Define the "Implements" variables
     I = [[z3.Bool("i_{}_{}".format(m, j)) for j in range(num_policies)]
          for m in range(num_nodes)]
-    print(I)
+    # print(I)
 
     # Define the "Exists" variables
     X = [z3.Bool("x_{}".format(m)) for m in range(num_nodes)]
-    print(X)
+    # print(X)
 
     # Define the "Executes" variables
     E = [[[z3.Bool("e_{}_{}_{}".format(i, j, m)) for m in range(num_nodes)]
           for j in range(num_policies)] for i in range(num_req_contexts)]
-    print(E)
+    # print(E)
 
     # Define the solver
     s = z3.Solver()
@@ -163,7 +242,7 @@ def main():
         pen_set, ult_set = get_policy_impls(policy["context"], appl_edges)
         pen_set = [appl_nodes.index(n) for n in pen_set]
         ult_set = [appl_nodes.index(n) for n in ult_set]
-        print(policy, pen_set, ult_set)
+        print("Possible impl for policy:", policy, pen_set, ult_set)
         alpha = z3.BoolVal(True)
         for m in pen_set:
             alpha = z3.And(I[m][j], alpha)
@@ -230,13 +309,6 @@ def main():
                         executions.append(
                             (all_req_contexts_list[i], j, appl_nodes[m]))
         print(executions)
-
-        # Check solution - Print B[2] and E[2] for the solution
-        # for j in range(num_policies):
-        #     print("B[2][{}] = {}".format(j, model.evaluate(B[2][j])))
-        #     for m in range(num_nodes):
-        #         print("E[2][{}][{}] = {}".format(j, m,
-        #                                             model.evaluate(E[2][j][m])))
 
 
 if __name__ == "__main__":
